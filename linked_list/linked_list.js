b96e0b6e6c7d9588382671614d5a485e3cd128e4
Igor Wessel <igor_wessel@hotmail.com>
2021-03-24 10:40:11 -0300

    refactor: class Node in another file

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -1,29 +1,7 @@
 /*
     Lista Encadeada (Lista com Alocação Dinamica) (ENG: Linked List)
 */
-
-class Node {
-  constructor(value) {
-    this._value = value;
-    this._next = null;
-  }
-
-  get next() {
-    return this._next;
-  }
-
-  get value() {
-    return this._value;
-  }
-
-  set value(newValue) {
-    this._value = newValue;
-  }
-
-  set next(node) {
-    this._next = node;
-  }
-}
+const Node = require('./node');
 
 class LinkedList {
   constructor() {
