d4ea7e3a7f92382589a71d7914e0a48d00bd5e4c
Igor Wessel <igor_wessel@hotmail.com>
2021-03-24 12:04:27 -0300

    feat: queue data structure

---
+++ queue/queue.js
@@ -0,0 +1,73 @@
+/*
+    First In First Out (FIFO)
+*/
+const Node = require('../node');
+
+class Queue {
+  constructor() {
+    this._first = null;
+    this._last = null;
+    this._size = 0;
+  }
+
+  get length() {
+    return this._size;
+  }
+
+  get values() {
+    // O(N)
+    let pointer = this._first;
+    const values = [];
+    while (pointer) {
+      values.push(pointer.value);
+      pointer = pointer.next;
+    }
+    return values;
+  }
+
+  toString() {
+    // O(N)
+    let pointer = this._first;
+    let string = '';
+    while (pointer) {
+      string += `${pointer.value},`;
+      pointer = pointer.next;
+    }
+    return string;
+  }
+
+  push(elem) {
+    //O(1)
+    const node = new Node(elem);
+    if (!this._first && !this._tail) {
+      this._first = node;
+      this._last = node;
+    } else {
+      this._last.next = node;
+      this._last = node;
+    }
+    this._size += 1;
+  }
+
+  pop() {
+    //O(1)
+    if (this._first) {
+      const node = this._first.next;
+      const removed = this._first.value;
+      this._first = node;
+      this._size -= 1;
+      return removed;
+    }
+    throw new Error('queue is empty');
+  }
+
+  peek() {
+    //O(1)
+    if (this._first) {
+      return this._first.value;
+    }
+    throw new Error('queue is empty');
+  }
+}
+
+module.exports = Queue;
---
+++ queue/queue.test.js
@@ -0,0 +1,59 @@
+const { test, expect } = require('@jest/globals');
+const Queue = require('./queue');
+
+const mockedQueue = new Queue();
+mockedQueue.push(1);
+mockedQueue.push(2);
+
+test('create a empty list', () => {
+  let a = new Queue();
+  expect(a.length).toBe(0);
+});
+
+test('should return values in queue, should return [1, 2] ', () => {
+  expect(mockedQueue.values).toEqual([1, 2]);
+});
+
+test('method push: add a element in queue when queue is empty, first and last element is the same', () => {
+  let a = new Queue();
+  a.push(1);
+  expect(a.length).toBe(1);
+  expect(a._first._value).toBe(1);
+  expect(a._last._value).toBe(1);
+});
+
+test('method push: add a element in queue when queue have elements, the first element dont change only last element', () => {
+  let a = new Queue();
+  a.push(1);
+  a.push(2);
+  expect(a.length).toBe(2);
+  expect(a._first._value).toBe(1);
+  expect(a._last._value).toBe(2);
+});
+
+test('method pop: remove first element in queue, 2,3,4 should remove 2', () => {
+  expect(mockedQueue.pop()).toBe(1);
+});
+
+test('method pop: remove element when queue is empty throw a error', () => {
+  const a = new Queue();
+  expect(() => {
+    a.pop();
+  }).toThrowError('queue is empty');
+});
+
+test('method peek: return first element in queue, 2,3,4 should return 2', () => {
+  expect(mockedQueue.peek()).toBe(2);
+});
+
+test('method peek: if queue is empty throw a error', () => {
+  const a = new Queue();
+  expect(() => {
+    a.pop();
+  }).toThrowError('queue is empty');
+});
+
+test('method toString: return a string "2, "', () => {
+  const string = mockedQueue.toString();
+  expect(string).toBe('2,');
+});
